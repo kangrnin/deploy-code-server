d33c9507cda8e8b36466827acde48d2dde64e1df
Kangmin Seo <kangrnin@gmail.com>
2021-06-17 23:29:35 +0900

    Update entrypoint.sh

--- deploy-container/entrypoint.sh
+++ deploy-container/entrypoint.sh
@@ -6,11 +6,6 @@ PREFIX="deploy-code-server"
 
 mkdir -p $START_DIR
 
-# function to clone the git repo or add a user's first file if no repo was specified.
-project_init () {
-    [ -z "${GIT_REPO}" ] && echo "[$PREFIX] No GIT_REPO specified" && echo "Example file. Have questions? Join us at https://community.coder.com" > $START_DIR/coder.txt || git clone $GIT_REPO $START_DIR
-}
-
 # add rclone config and start rclone, if supplied
 if [[ -z "${RCLONE_DATA}" ]]; then
     echo "[$PREFIX] RCLONE_DATA is not specified. Files will not persist"
